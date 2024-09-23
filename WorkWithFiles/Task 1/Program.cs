7f93497cb703e2d8a32f38b9d1d0b0bc7035ca3e
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-09-23 22:53:22 +0700

    работа над заданием 1

--- WorkWithFiles/Task 1/Program.cs
+++ WorkWithFiles/Task 1/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+string tempFile = @"C:\\Users\Александр\Desktop\Test_papka\dada.txt"; // Получаем путь до папки.
+var fileInfo = new FileInfo(tempFile); // Создаем объект класса FileInfo.
+
+
+try
+{
+    //Удаляем ранее созданный файл.
+    fileInfo.Delete();
+    Console.WriteLine($"{tempFile} удален.");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Ошибка: {e}");
+}
